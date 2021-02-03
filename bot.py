3d086df4e0712ffc29fd7c694deec9fbdb59b545
Shevchenko Sergey <rootsergio@gmail.com>
2021-02-03 17:55:14 +0300

    Подключил MemoryStorage в качестве хранилища состояний

--- bot.py
+++ bot.py
@@ -1,8 +1,9 @@
 import logging
 from aiogram import Bot, Dispatcher
 from config.config import settings
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 logging.basicConfig(level=logging.INFO)
 
 bot = Bot(token=settings.BOT_TOKEN)
-dp = Dispatcher(bot)
+dp = Dispatcher(bot, storage=MemoryStorage())
--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,3 +1,3 @@
 from . import general_commands
-# from . import hashes_commands
+from . import hashes_commands
 from . import default_handler
